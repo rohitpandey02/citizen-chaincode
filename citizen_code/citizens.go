fba597806fce2de9df9225c602d3f987d0892181
rohitpandey02 <rohit.pandey@in.ibm.com>
2017-05-06 19:13:55 +0530

    Role Fix

--- citizen_code/citizens.go
+++ citizen_code/citizens.go
@@ -133,13 +133,19 @@ func (t *SimpleChaincode) get_caller_data(stub shim.ChaincodeStubInterface) (str
 
 	user, err := t.get_username(stub)
 
-	role, err := t.check_role(stub)
+	if err != nil { return "", "", err }
+
+	ecert, err := t.get_ecert(stub, user);
+
+  if err != nil { return "", "", err }
+
+	/*role, err := t.check_role(stub)
 
 	if err != nil {
 		return "", "", err
-	}
+	}*/
 
-	return user, role, nil
+	return user, string(ecert[:]), nil
 }
 
 func (t *SimpleChaincode) retrieve_ID(stub shim.ChaincodeStubInterface, PersonID string) (Citizen, error) {
