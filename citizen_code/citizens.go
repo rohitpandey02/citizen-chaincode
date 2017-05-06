963b280c863f4a4b08e6e30e37494ba35d176416
rohitpandey02 <rohit.pandey@in.ibm.com>
2017-05-06 19:16:35 +0530

    healthrecordfix

--- citizen_code/citizens.go
+++ citizen_code/citizens.go
@@ -427,10 +427,17 @@ func (t *SimpleChaincode) add_healthrecord(stub shim.ChaincodeStubInterface, c C
 
 	err := json.Unmarshal([]byte(healthrecord_json), &healthrecord)
 
+	if err != nil {
+		fmt.Printf("ADD_HEALTHRECORD: Error in JSON conversion: %s", err)
+		return nil, errors.New("Invalid JSON object:" + healthrecord_json)
+	}
+
 	if caller_role == HEALTHCARE_ADMIN || caller_role == HEALTHCARE_USER {
-
+		/*if c.PersonHealth == {
+			var healthrecords []Health
+			c.PersonHealth = healthrecords
+		}*/
 		c.PersonHealth = append(c.PersonHealth, healthrecord)
-
 	} else {
 		return nil, errors.New(fmt.Sprint("Permission denied. add_healthrecord"))
 	}
